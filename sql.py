d2825ca9dde0c98784b1149e6f73131df805e75d
Wolans <98850912+Wolans@users.noreply.github.com>
2023-02-18 01:22:34 +0300

    Update 1.22

--- sql.py
+++ sql.py
@@ -1,5 +1,8 @@
 import pyodbc
 import threading
+import PIL.Image as Image
+import io
+
 
 server = 'SQL8004.site4now.net'
 database = 'db_a93836_invest'
@@ -7,6 +10,12 @@ username = 'db_a93836_invest_admin'
 password = 'elmelm327327'
 cnxn = pyodbc.connect(
     'DRIVER={ODBC Driver 18 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';ENCRYPT=yes;UID=' + username + ';PWD=' + password)
+
+
+def img(path):
+    io_bytes = io.BytesIO(path)
+    i = Image.open(io_bytes)
+    return i
 
 
 def check_usr(user_id):
@@ -30,13 +39,13 @@ def favorite(user_id):
             states_id.append(row[1])
     cursor = cnxn.cursor()
     cursor.execute('select * from dbo.RealEstates')
-    info = [
-        {
-        }
-    ]
+    info = dict({
+
+    })
     for row in cursor.fetchall():
         if row[0] in states_id:
             l = {
+                'id_state': row[0],
                 'address': row[1],
                 'pr2m': row[4],
                 'area': row[5],
@@ -50,10 +59,16 @@ def favorite(user_id):
                 'price': float(row[4]) * float(row[5]),
                 'image': ''
             }
-            info.append(l)
+            info[row[0]] = l
     cursor.execute('select _Image, StateId from dbo.Images')
-    # for row in cursor.fetchall():
-
+    a = ''
+    for row in cursor.fetchall():
+        if row[1] != a:
+            a = row
+            if a in info.keys():
+                l = info[row[1]]
+                l['image'] = img(row[0])
+                info[row[1]] = l
     return info
 
 
@@ -61,12 +76,17 @@ def check():
     # threading.Timer(1800.0, Check)
     cursor = cnxn.cursor()
     cursor.execute('select UserId, StateId, IsChanged from dbo.Favorites')
-    lib = {
-
-    }
-    # for row in cursor.fetchall():
-    #  if row[2]:
-
+    lib = dict([])
+    for row in cursor.fetchall():
+        if row[2]:
+            if row[0] in lib.keys():
+                l = lib[row[0]]
+                l.append(row[1])
+                lib[row[0]] = l
+            else:
+                l = []
+                l.append(row[1])
+                lib[row[0]] = l
     return lib
 
 # 5708104256
